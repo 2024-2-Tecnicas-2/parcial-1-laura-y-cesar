017cf26ff34458df0f7e0aab66413bf2f79d5870
Jhon doe <Jhon.doe1112@gmail.com>
2024-09-16 21:12:38 -0500

    Clase Pedido-1

--- src/main/java/com/mycompany/parcial1/Principal.java
+++ src/main/java/com/mycompany/parcial1/Principal.java
@@ -4,5 +4,30 @@ public class Principal {
 
     public static void main(String[] args) {
         // TODO: Aquí ingresas tu código para iniciar la aplicación.
+public class Pedido {
+    private Cliente cliente;
+    private ArrayList<Producto>productosSolicitados;
+    private ArrayList<Integer>cantidadesSolicitadas;
+    
+    public Pedido(Cliente cliente){
+    this.cliente = cliente;
+    this.productosSolicitados = new ArrayList<>();
+    this.cantidadesSolicitadas = new ArrayList<>();  
+    }
+    
+    public void mostrarPedido(){
+        System.out.println("Cliente: " + cliente.getNombre());
+        System.out.println("Direccion: " + cliente.getDireccion());
+        System.out.println("Correo: " + cliente.getCorreoElectronico());
+        System.out.println("Productos solicitados: ");
+            for(int i = 0; i < productosSolicitados.size(); i++) {
+                Producto producto = productosSolicitados.get(i);
+                int cantidad = cantidadesSolicitadas.get(i);
+                    System.out.printLn("- " + producto.getNombre() + " -Cantidad: " + cantidad);
+            }      
+    }
+}
+
+
     }
 }
